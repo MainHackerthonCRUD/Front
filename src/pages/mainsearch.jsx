42474ad9b1e11c5854634ea96c63187b059f063f
LYZAcell <applelgy@naver.com>
2024-07-20 23:34:52 +0900

    검색창 연결 및 mockdata확인중

--- src/pages/mainsearch.jsx
+++ src/pages/mainsearch.jsx
@@ -1,17 +1,36 @@
 import styled from 'styled-components';
 import api from "../api";
+import React, {useRef} from "react";
 
 export default function SearchBox(){
+    const inputRef = useRef(null);
+
+    const handleSearch = async () => {
+        const searchText = inputRef.current.value;
+    
+        if (searchText) {
+            try{
+                const response = await api.get(`/board/name/${searchText}`);
+                console.log(response.data);
+            }catch(error){
+                console.error("에러내역:", error);
+            }
+        } 
+
+    };
 
 
     return(
         <>
         <input type = "search"
         placeholder = "산부인과 후기 검색하기"
-        className = 'SearchInput'/>
+        className = 'SearchInput'
+        ref = {inputRef}
+        />
 
         <button
-        id = 'search-Btn'>
+        id = 'search-Btn'
+        onClick={handleSearch}>
          검색
         </button>
         </>
