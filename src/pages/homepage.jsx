d2e88fd1aa9a98cd041f4fcda604406bba34621c
LYZAcell <applelgy@naver.com>
2024-07-21 18:37:41 +0900

    메인 정리 및 로고 수정, 검색창 css"

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -5,15 +5,41 @@ export default function HomePage() {
 
     return (
       <>
-      <p>HOME: JSON 랜더링</p>
-      <br/>
-      <Logo> 마다닥! </Logo>
+      <MainContainer>
+      
+      <LogoSentence>빠르게 산부인과를 가야할 때</LogoSentence>
+      <Logo> 마다닥 </Logo>
+      
       <SearchBox/>
+      </MainContainer>
       </>
     );
   }  
 
+const MainContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    height: 60vh;
+`
+
+const LogoSentence = styled.p`
+  color : #7d7c7c;
+  font-size: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
 const Logo  = styled.h1`
-  color : #FFAA00
+  color : #FFAA00;
+  font-size: 80px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
 `
   
