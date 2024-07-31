1cddb8068c4d41722f339d1ed06a4a44fa7f2155
wh0land <cliee0730@gamil.com>
2024-07-31 09:42:44 +0900

    review detail layout editing

--- src/pages/reviewdetail.jsx
+++ src/pages/reviewdetail.jsx
@@ -5,42 +5,58 @@ import styled from "styled-components";
 
 // 고민.. 병원에 대해 댓글식으로 존재할거라면 리뷰 상세 조회 페이지의 필요가 있는가?
 export default function ReviewDetail() {
-    const { postid } = useParams();
-    const [detail, setDetail] = useState(null);
-    const [error, setError] = useState(null);
+    // const { postid } = useParams();
+    // const [detail, setDetail] = useState(null);
+    // const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const Detail = async () => {
-            try {
-                const res = await api.get(`/board/home/${postid}`);
-                setDetail(res.data);
-            } catch (error) {
-                setError(error.message);
-            }
-        };
-        Detail();
-    }, [postid]);
+    // // useEffect(() => {
+    //     const Detail = async () => {
+    //         try {
+    //             const res = await api.get(`/board/home/${postid}`);
+    //             setDetail(res.data);
+    //         } catch (error) {
+    //             setError(error.message);
+    //         }
+    //     };
+    //     Detail();
+    // }, [postid]);
 
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
+    // if (error) {
+    //     return <div>Error: {error}</div>;
+    // }
 
-    if (!detail) {
-        return <div>Loading...</div>;
-    }
+    // if (!detail) {
+    //     return <div>Loading...</div>;
+    // }
+
+    // return (
+    //     <div>
+    //         <ReviewPageWrapper>
+    //         <h2>리뷰상세</h2>
+    //         <ReviewItemWrapper>
+    //             <h3>{detail.user}</h3>
+    //             <div>
+    //             <InfoHospital>{detail.hospital_name}</InfoHospital>
+    //             <InfoHospital>{detail.region}</InfoHospital>
+    //             </div>
+    //             <p>{detail.textbox}</p>
+    //         </ReviewItemWrapper>
+    //         </ReviewPageWrapper>
+    //     </div>
+    // );
 
     return (
         <div>
             <ReviewPageWrapper>
-            <h2>리뷰상세</h2>
-            <ReviewItemWrapper>
-                <h3>{detail.user}</h3>
-                <div>
-                <InfoHospital>{detail.hospital_name}</InfoHospital>
-                <InfoHospital>{detail.region}</InfoHospital>
-                </div>
-                <p>{detail.textbox}</p>
-            </ReviewItemWrapper>
+                <h2>리뷰상세</h2>
+                <ReviewItemWrapper>
+                    <h3>닉네임</h3>
+                    <div>
+                        <InfoHospital>병원이름</InfoHospital>
+                        <InfoHospital>구</InfoHospital>
+                    </div>
+                    <p>리뷰텍스트어쩌구저쩌구</p>
+                </ReviewItemWrapper>
             </ReviewPageWrapper>
         </div>
     );
@@ -55,9 +71,8 @@ const ReviewPageWrapper = styled.div`
     max-width: 650px;
     gap: 10px;
     background-color: #f7f7f7;
-    border-radius: 20px;
+    border-radius: 10px;
     padding: 20px 20px 20px 20px;
-    margin-top: 80px;
 
     h2 {
         margin-bottom: 30px;
