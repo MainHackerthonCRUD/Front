38e240b4344f0b0863f171b6dcac3ceeff3feacf
wh0land <cliee0730@gamil.com>
2024-07-31 17:54:50 +0900

    review detail link updated

--- src/pages/reviewdetail.jsx
+++ src/pages/reviewdetail.jsx
@@ -3,64 +3,47 @@ import { useParams } from "react-router-dom";
 import api from "../api";
 import styled from "styled-components";
 
-// 고민.. 병원에 대해 댓글식으로 존재할거라면 리뷰 상세 조회 페이지의 필요가 있는가?
 export default function ReviewDetail() {
-    // const { postid } = useParams();
-    // const [detail, setDetail] = useState(null);
-    // const [error, setError] = useState(null);
+    const { hospitalid, postid } = useParams();
+    const [detail, setDetail] = useState(null);
+    const [error, setError] = useState(null);
 
-    // // useEffect(() => {
-    //     const Detail = async () => {
-    //         try {
-    //             const res = await api.get(`/board/home/${postid}`);
-    //             setDetail(res.data);
-    //         } catch (error) {
-    //             setError(error.message);
-    //         }
-    //     };
-    //     Detail();
-    // }, [postid]);
+    useEffect(() => {
+        const Detail = async () => {
+            try {
+                const res = await api.get(`/board/review/${hospitalid}/${postid}`);
+                setDetail(res.data);
+            } catch (error) {
+                setError(error.message);
+            }
+        };
+        Detail();
+    }, [hospitalid, postid]);
 
-    // if (error) {
-    //     return <div>Error: {error}</div>;
-    // }
+    if (error) {
+        return <div>Error: {error}</div>;
+    }
 
-    // if (!detail) {
-    //     return <div>Loading...</div>;
-    // }
-
-    // return (
-    //     <div>
-    //         <ReviewPageWrapper>
-    //         <h2>리뷰상세</h2>
-    //         <ReviewItemWrapper>
-    //             <h3>{detail.user}</h3>
-    //             <div>
-    //             <InfoHospital>{detail.hospital_name}</InfoHospital>
-    //             <InfoHospital>{detail.region}</InfoHospital>
-    //             </div>
-    //             <p>{detail.textbox}</p>
-    //         </ReviewItemWrapper>
-    //         </ReviewPageWrapper>
-    //     </div>
-    // );
+    if (!detail) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div>
             <ReviewPageWrapper>
                 <h2>리뷰 상세</h2>
                 <ReviewItemWrapper>
-                    <h3>병원 이름|detail.title</h3>
+                    <h3>병원 이름|{detail.title}</h3>
                 </ReviewItemWrapper>
                 <ReviewItemWrapper>
                     <div>
-                        <InfoHospital>detail.star</InfoHospital>
+                        <InfoHospital>{detail.star}</InfoHospital>
                     </div>
                     <div>
-                        <InfoHospital>detail.nickname</InfoHospital>
-                        <InfoHospital>detail.created_at</InfoHospital>
+                        <InfoHospital>{detail.nickname}</InfoHospital>
+                        <InfoHospital>{detail.created_at}</InfoHospital>
                     </div>
-                    <p>detail.body</p>
+                    <p>{detail.body}</p>
                 </ReviewItemWrapper>
             </ReviewPageWrapper>
         </div>
